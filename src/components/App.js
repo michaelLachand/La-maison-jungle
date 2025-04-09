fa64209fa654fb3d5f793a7aa32f40376d1add8a
Michael Lachand <lachand_michael@hotmail.com>
2025-04-09 19:08:23 +0200

    Ajout du component Footer et CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import Banner from "./Banner";
 import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
+import Footer from "./Footer";
+import logo from '../assets/logo.png'
 
 function App() {
-  return (
-      <div>
-        <Banner />
-        <Cart />
-        <ShoppingList />
-      </div>
-  )
+    return (
+        <div>
+            <Banner>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>La maison jungle</h1>
+            </Banner>
+            <Cart />
+            <ShoppingList />
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
